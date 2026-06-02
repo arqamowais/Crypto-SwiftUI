3ca689d4ecdd9805ecb7373e2894cacf42e1bdf8
Arqam Owais <arqam.owais@veripark.com>
2026-06-03 02:28:41 +0500

    Initial commit

---
+++ Crypto-SwiftUI/Crypto_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_SwiftUIApp.swift
+//  Crypto-SwiftUI
+//
+//  Created by Arqam Owais on 03/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
